8c379019b69e977764077315c11f4a9b588555e1
Celestine <macstine@gmail.com>
2022-12-13 22:30:37 +0100

    updated app to use jwt for endpoint authentication

--- src/app.py
+++ src/app.py
@@ -1,12 +1,78 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, abort, jsonify, request, make_response
 from flask_cors import CORS, cross_origin
-
-from generator import *
-
+from functools import wraps
+import jwt
+import logging
 
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '$Celestine$'
+
 CORS(app, resources={r"*/api/*": {"origins": "*"}})
+
+logging.basicConfig(
+    filename='app.log', filemode='w', format='%(levelname)s in %(module)s: %(message)s', 
+    datefmt='%d-%b-%y %H:%M:%S'
+)
+
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+def get_token_auth_header():
+    """Obtains the Access Token from the Authorization Header
+    """
+    auth = request.headers.get('Authorization', None)
+    if not auth:
+        raise AuthError({
+            'code': 'authorization_header_missing',
+            'description': 'Authorization header is expected.'
+        }, 401)
+
+    parts = auth.split()
+    if parts[0].lower() != 'bearer':
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must start with "Bearer".'
+        }, 401)
+
+    elif len(parts) == 1:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Token not found.'
+        }, 401)
+
+    elif len(parts) > 2:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must be bearer token.'
+        }, 401)
+
+    return parts[1]
+    
+def token_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        token = get_token_auth_header()
+        if 'x-access-tokens' in request.headers:
+            #token = request.headers['x-access-tokens']
+            token = request.args.get('token')
+
+        if not token:
+            return jsonify({'message': 'a valid token is missing'})
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+            current_user = User.query.filter_by(public_id=data['public_id']).first()
+        except Exception:
+            return jsonify({'message': 'token is invalid'})
+
+        return f(current_user, *args, **kwargs)
+
+    return decorator
+
 
 
 @app.route('/')
